d4e1fab1a0354c5e96d3f43f982e23ec9b91f89c
Arzoka <114647680+Arzoka@users.noreply.github.com>
2022-12-12 23:46:28 +0100

    Updated all

--- Website/main.js
+++ Website/main.js
@@ -1,1 +1,13 @@
-//woeoeoe code
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function(){
+    var st = window.pageYOffset || document.documentElement.scrollTop; 
+    if (st > lastScrollTop){
+        console.log('scrolldown')
+    //document.getElementById("bar").style.animation = "barup 0.5s";
+    } else {
+        console.log('scrollup')
+    //document.getElementById("bar").style.animation = "bardown 0.5s";
+   }
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
