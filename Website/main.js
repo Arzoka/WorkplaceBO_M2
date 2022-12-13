7550acb3040e72448db9bc4443b3f226f2e20d40
Arzoka <114647680+Arzoka@users.noreply.github.com>
2022-12-13 09:53:00 +0100

    webdev

--- Website/main.js
+++ Website/main.js
@@ -1,13 +1,10 @@
-var lastScrollTop = 0;
-
-window.addEventListener("scroll", function(){
-    var st = window.pageYOffset || document.documentElement.scrollTop; 
-    if (st > lastScrollTop){
-        console.log('scrolldown')
-    //document.getElementById("bar").style.animation = "barup 0.5s";
-    } else {
-        console.log('scrollup')
-    //document.getElementById("bar").style.animation = "bardown 0.5s";
-   }
-   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-}, false);
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("bar").style.top = "0";
+  } else {
+    document.getElementById("bar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
