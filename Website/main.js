b05965e73aefc3bb1c70678f39e4e832e1b4d8b8
Arzoka <114647680+Arzoka@users.noreply.github.com>
2022-12-14 12:24:13 +0100

    more webdev

--- Website/main.js
+++ Website/main.js
@@ -1,4 +1,5 @@
 var prevScrollpos = window.pageYOffset;
+
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
@@ -8,3 +9,41 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+
+function checkInputFocus() {
+  var s_input = document.getElementById("search-input");
+  var s_icon = document.getElementById("s-icon");
+  var s_bar = document.getElementById("s-bar")
+
+  if (document.activeElement == s_input){
+    console.log("has focus");
+    s_bar.style.width = "20%";
+    //s_bar.style.top = "-10%";
+    s_bar.style.filter = "drop-shadow(0px -3px 5px #FFFFFF) drop-shadow(0px 3px 5px #FFFFFF)"
+    if (s_input && s_input.value) {
+      s_icon.style.opacity = 0;
+    }
+    else {
+      s_icon.style.opacity = 1;
+    }
+    
+  }
+  else {
+    s_bar.style.filter = "drop-shadow(0px -3px 5px #000000) drop-shadow(0px 3px 5px #000000)"
+    s_bar.style.width = "12.5%";
+    //s_bar.style.top = "0";  
+
+    
+    if (s_input && s_input.value) {
+      s_icon.style.opacity = 0;
+    }
+    else{
+      s_icon.style.opacity = 1;
+    }
+   
+  }
+}
+
+
+setInterval(checkInputFocus, 5);
