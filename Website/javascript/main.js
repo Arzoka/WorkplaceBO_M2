4169f1a4768f9b96fed01d32c185af7a5ca61e80
Hadeer Abou <nuggie@MacBook-Air.local>
2023-01-27 21:18:00 +0100

    highlights done

--- Website/javascript/main.js
+++ Website/javascript/main.js
@@ -70,7 +70,7 @@ window.onload = function() {
     setCookie("visited", "yes", null);
     }
 }
-if (window.location.href.includes("home.html")) {
+if (window.location.href.includes("home.html") || window.location.href.includes("game.html")) {
     const swiper = new Swiper('.swiper', {
         autoplay: {
             delay: 3000,
